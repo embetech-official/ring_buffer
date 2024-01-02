a1fe531df5b887406c2a3d71086c09b231490a16
Jan Macheta <jan.macheta@embe.tech>
2024-01-02 19:40:01 +0100

    initial commit

---
+++ src/include/embetech/ring_buffer_16bit.h
@@ -0,0 +1,125 @@
+/**
+@file
+@license   $License$
+@copyright $Copyright$
+@version   $Revision$
+@purpose   General-purpose ring buffer
+@brief     Ring Buffer API
+
+This module implements a basic, general-purpose ring buffer.
+*/
+
+#ifndef RING_BUFFER_16BIT_H_
+#define RING_BUFFER_16BIT_H_
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/** @addtogroup DRIVERS
+ * @{
+ */
+
+/** @defgroup RING_BUFFER This module delivers a basic, general-purpose ring buffer.
+ * @{
+ */
+
+/** Structure describing the ring buffer. */
+typedef struct {
+  /// pointer to the area of memory, where data will be stored
+  uint16_t *dataBuffer;
+  /// size of the data buffer (in bytes)
+  size_t dataBufferSize;
+  /// number of elements currently stored within the buffer
+  size_t count;
+  /// pointer to the next element in the buffer that will be written
+  uint16_t *head;
+  /// pointer to the next element in the buffer that will be read
+  uint16_t *tail;
+} RingBuffer16Bit;
+
+/**
+ * Initializes the given ring buffer structure.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @param dataBuffer pointer to a location in memory, where the ring buffer data will be stored
+ * @param dataBufferSize size in bytes of the dataBuffer
+ * @return true if all arguments are valid and the ring buffer is initialized successfully, false otherwise
+ */
+bool RingBuffer16Bit_Init(RingBuffer16Bit *RingBuffer16Bit, uint16_t *dataBuffer, size_t dataBufferSize);
+
+/**
+ * Clears contents of the given ring buffer.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @return true if the ring buffer is cleared successfully, false otherwise
+ */
+bool RingBuffer16Bit_Clear(RingBuffer16Bit *RingBuffer16Bit);
+
+/**
+ * Checks if the given ring buffer is empty.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @return true if the ring buffer holds no data, false otherwise
+ */
+bool RingBuffer16Bit_IsEmpty(RingBuffer16Bit const *RingBuffer16Bit);
+
+/**
+ * Gets the length (in bytes) of the data stored in the given ring buffer.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @return length (in bytes) of the data stored in the ring buffer
+ */
+size_t RingBuffer16Bit_GetLen(RingBuffer16Bit const *RingBuffer16Bit);
+
+/**
+ * Returns the capacity (in bytes) of the given buffer.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @return capacity (in bytes) of the ring buffer (how much characters can it store)
+ */
+size_t RingBuffer16Bit_GetCapacity(RingBuffer16Bit const *RingBuffer16Bit);
+
+/**
+ * Returns the space (in bytes) left in the given buffer.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @return space (in bytes) of the ring buffer (how much characters can be still written to it)
+ */
+size_t RingBuffer16Bit_GetSpace(RingBuffer16Bit const *RingBuffer16Bit);
+
+/**
+ * Appends a single character to the ring buffer. The stored data length will be
+ * increased by 1.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @param c character to append
+ * @return true if the character was added successfully, false otherwise
+ */
+bool RingBuffer16Bit_PutChar(RingBuffer16Bit *RingBuffer16Bit, uint16_t c);
+
+/**
+ * Pulls out a single character from the ring buffer. The stored data length will be
+ * decreased by 1.
+ *
+ * @param RingBuffer16Bit pointer to a \ref RingBuffer16Bit structure
+ * @param c pointer to a place where character will be stored
+ * @return true if the character was pulled out successfully, false otherwise
+ */
+bool RingBuffer16Bit_GetChar(RingBuffer16Bit *RingBuffer16Bit, uint16_t *c);
+
+/**
+ *   @}
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/ring_buffer_16bit.c
@@ -0,0 +1,90 @@
+/**
+@file
+@license   $License$
+@copyright $Copyright$
+@version   $Revision$
+@purpose   General-purpose ring buffer
+@brief     Ring Buffer implementation
+
+This module implements a basic, general-purpose ring buffer.
+*/
+
+#include "ring_buffer_16bit.h"
+
+bool RingBuffer16Bit_Init(RingBuffer16Bit *ringBuffer, uint16_t *dataBuffer, size_t dataBufferSize) {
+  if((ringBuffer) && (dataBuffer) && (dataBufferSize > 0)) {
+    ringBuffer->dataBuffer = dataBuffer;
+    ringBuffer->dataBufferSize = dataBufferSize;
+    ringBuffer->count = 0;
+    ringBuffer->head = dataBuffer;
+    ringBuffer->tail = dataBuffer;
+    return true;
+  }
+
+  return false;
+}
+
+bool RingBuffer16Bit_Clear(RingBuffer16Bit *ringBuffer) {
+  if(ringBuffer) {
+    ringBuffer->count = 0;
+    ringBuffer->head = ringBuffer->dataBuffer;
+    ringBuffer->tail = ringBuffer->dataBuffer;
+    return true;
+  }
+  return false;
+}
+
+bool RingBuffer16Bit_IsEmpty(RingBuffer16Bit const *ringBuffer) {
+  if(ringBuffer) {
+    return (ringBuffer->count == 0);
+  }
+
+  return true;
+}
+
+size_t RingBuffer16Bit_GetLen(RingBuffer16Bit const *ringBuffer) {
+  if(ringBuffer) {
+    return ringBuffer->count;
+  }
+  return 0;
+}
+
+size_t RingBuffer16Bit_GetCapacity(RingBuffer16Bit const *ringBuffer) {
+  if(ringBuffer) {
+    return ringBuffer->dataBufferSize;
+  }
+  return 0;
+}
+
+size_t RingBuffer16Bit_GetSpace(RingBuffer16Bit const *ringBuffer) {
+  if(ringBuffer) {
+    return ringBuffer->dataBufferSize - ringBuffer->count;
+  }
+  return 0;
+}
+
+bool RingBuffer16Bit_PutChar(RingBuffer16Bit *ringBuffer, uint16_t c) {
+  if((ringBuffer) && (ringBuffer->count < ringBuffer->dataBufferSize)) {
+    *ringBuffer->head = c;
+    ringBuffer->count++;
+    ringBuffer->head++;
+    if(ringBuffer->head >= ringBuffer->dataBuffer + ringBuffer->dataBufferSize) {
+      ringBuffer->head = ringBuffer->dataBuffer;
+    }
+    return true;
+  }
+  return false;
+}
+
+bool RingBuffer16Bit_GetChar(RingBuffer16Bit *ringBuffer, uint16_t *c) {
+  if((ringBuffer) && (c) && (ringBuffer->count)) {
+    *c = *ringBuffer->tail;
+    ringBuffer->count--;
+    ringBuffer->tail++;
+    if(ringBuffer->tail >= ringBuffer->dataBuffer + ringBuffer->dataBufferSize) {
+      ringBuffer->tail = ringBuffer->dataBuffer;
+    }
+    return true;
+  }
+  return false;
+}
