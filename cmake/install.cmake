4916e8ed47903527a9c7f792177f7849e79d0eb0
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2026-01-15 15:28:23 +0100

    fix version config install filepath

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -6,13 +6,15 @@ set(METADATA_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/ring_buffer)
 install(TARGETS ring_buffer EXPORT ring_buffer-targets ARCHIVE FILE_SET HEADERS)
 install(EXPORT ring_buffer-targets NAMESPACE embetech:: DESTINATION ${METADATA_DIR})
 
-write_basic_package_version_file(ring_buffer-version.cmake COMPATIBILITY SameMajorVersion)
+write_basic_package_version_file(ring_buffer-config-version.cmake COMPATIBILITY SameMajorVersion)
 
 configure_package_config_file(
   ${CMAKE_CURRENT_LIST_DIR}/ring_buffer-config_template.cmake ${CMAKE_CURRENT_BINARY_DIR}/ring_buffer-config.cmake INSTALL_DESTINATION ${METADATA_DIR}
   NO_CHECK_REQUIRED_COMPONENTS_MACRO
 )
 
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ring_buffer-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/ring_buffer-version.cmake DESTINATION ${METADATA_DIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ring_buffer-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/ring_buffer-config-version.cmake
+        DESTINATION ${METADATA_DIR}
+)
 
 install(SCRIPT ${CMAKE_CURRENT_LIST_DIR}/install_header_licenses.cmake)
