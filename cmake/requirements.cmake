0994d0dbca12e943dc79d37035f4b5dee9e3ee99
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2026-06-25 02:26:48 +0200

    fix cpm and gtest version

--- cmake/requirements.cmake
+++ cmake/requirements.cmake
@@ -1,9 +1,5 @@
-file(DOWNLOAD https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.42.0/CPM.cmake ${CMAKE_BINARY_DIR}/cmake/CPM.cmake
-     EXPECTED_HASH SHA256=2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a
-)
-
-include(${CMAKE_BINARY_DIR}/cmake/CPM.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
 if (RING_BUFFER_TESTS)
-  CPMAddPackage("gh:google/googletest#main")
+  CPMAddPackage("gh:google/googletest@1.17.0")
 endif ()
